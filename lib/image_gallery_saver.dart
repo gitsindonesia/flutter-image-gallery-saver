d0b1fd9a4bd0852cad5b8f3f0bc5ba7ad6686230
Dena Meidina <dena@gits.id>
2022-09-14 10:27:37 +0700

    refactor: add flutter lints

--- lib/image_gallery_saver.dart
+++ lib/image_gallery_saver.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
@@ -15,26 +14,29 @@ class ImageGallerySaver {
       {int quality = 80,
       String? name,
       bool isReturnImagePathOfIOS = false}) async {
-    assert(imageBytes != null);
-    final result =
-        await _channel.invokeMethod('saveImageToGallery', <String, dynamic>{
-      'imageBytes': imageBytes,
-      'quality': quality,
-      'name': name,
-      'isReturnImagePathOfIOS': isReturnImagePathOfIOS
-    });
+    final result = await _channel.invokeMethod(
+      'saveImageToGallery',
+      <String, dynamic>{
+        'imageBytes': imageBytes,
+        'quality': quality,
+        'name': name,
+        'isReturnImagePathOfIOS': isReturnImagePathOfIOS
+      },
+    );
     return result;
   }
 
   /// Save the PNG，JPG，JPEG image or video located at [file] to the local device media gallery.
-  static Future saveFile(String file, {String? name, bool isReturnPathOfIOS = false}) async {
-    assert(file != null);
+  static Future saveFile(String file,
+      {String? name, bool isReturnPathOfIOS = false}) async {
     final result = await _channel.invokeMethod(
-        'saveFileToGallery', <String, dynamic>{
-      'file': file,
-      'name': name,
-      'isReturnPathOfIOS': isReturnPathOfIOS
-    });
+      'saveFileToGallery',
+      <String, dynamic>{
+        'file': file,
+        'name': name,
+        'isReturnPathOfIOS': isReturnPathOfIOS
+      },
+    );
     return result;
   }
 }
