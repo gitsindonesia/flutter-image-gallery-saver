af3805ce60ebdcb21220ece410f745b6d38ff484
teddypangkat <teddy.pangkat@gmail.com>
2026-03-26 10:52:16 +0700

    Update ImageGallerySaverPlugin.kt
    remove registrar

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -15,7 +15,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
@@ -27,14 +26,6 @@ class ImageGallerySaverPlugin : FlutterPlugin, MethodCallHandler {
     private var applicationContext: Context? = null
     private var methodChannel: MethodChannel? = null
 
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val instance = ImageGallerySaverPlugin()
-            instance.onAttachedToEngine(registrar.context(), registrar.messenger())
-        }
-    }
 
     override fun onMethodCall(call: MethodCall, result: Result): Unit {
         when {
